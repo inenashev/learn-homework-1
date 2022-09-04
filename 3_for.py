9beec0fa146d7b28e1b0630525d855cefbf26fe5
Ivan Nenashev <inenashev@icloud.com>
2022-09-04 17:15:47 +0300

    Задание 3 - 1

--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,34 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+
+def sum_arr(arr):
+    return sum(arr)
+
+
+def mean_arr(arr):
+    return sum(arr)/len(arr)
+
+def sum_by_product(all_data):
+    print("-----Продажи по товарам------")
+    for d in all_data:
+        print(f"Продукт: {d['product']}: {sum_arr(d['items_sold'])}")
+        print()
+
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    data = [
+        {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+        {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+        {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+
+    sum_by_product(data)
+
+
 if __name__ == "__main__":
     main()
